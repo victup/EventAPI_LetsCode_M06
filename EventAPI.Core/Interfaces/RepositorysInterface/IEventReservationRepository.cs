3063d3e55a771d74c6b93c376940200d447da710
Víctor Gonçalves <38474570+victup@users.noreply.github.com>
2022-09-13 10:40:46 -0300

    Criação de classes em suas respectivas camadas

---
+++ EventAPI.Core/Interfaces/RepositorysInterface/IEventReservationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAPI.Core.Interfaces.RepositorysInterface
+{
+    public interface IEventReservationRepository
+    {
+    }
+}
